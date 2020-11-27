fa6acde78c1e35593c4c6b061d2fbaf4a6e2b3c7
nswdwy_yu <nswdwy_yu@163.com>
2020-11-27 15:50:09 +0800

    sac

---
+++ src/main/java/aa.java
@@ -0,0 +1,11 @@
+import sun.applet.Main;
+
+/**
+ * @author yycstart
+ * @create 2020-11-27 15:48
+ */
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
